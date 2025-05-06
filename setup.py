9ef4b1f890832de2b14a3015ba58c2d423de7fa4
Jeroen van der Heijden <jeroen@cesbit.com>
2025-05-06 09:07:50 +0200

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ try:
 except IOError:
     long_description = ''
 
-version = '1.0.0'  # Update version in bin/pywmitool as well
+version = '1.0.1'  # Update version in bin/pywmitool as well
 
 setup(
     name='pywmitool',
